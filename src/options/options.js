646d60ae2cc473bf09e41c06194ca530bcf5228c
gopinathshiva <sgopinath31@gmail.com>
2016-03-09 16:38:59 +0530

    adding notification support to extension

--- src/options/options.js
+++ src/options/options.js
@@ -2,8 +2,20 @@ chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.log){
             $('#logs').append('<p>'+request.log+'</p>');
+            var opt = {
+                type: "basic",
+                title: "PixelsTech Website Statistic Viewer",
+                message: "1,000,000",
+                iconUrl: "../../icons/icon.png"
+            };
+            chrome.notifications.create(getNotificationId(), opt, function() {});
         }
 });
+
+function getNotificationId() {
+  var id = Math.floor(Math.random() * 9007199254740992) + 1;
+  return id.toString();
+}
 
 function selectText(containerid) {
     if (document.selection) {
